0eb3db4481b6dcb5675acb91b13efc808e25fdb0
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2020-11-29 22:38:27 +0530

    Committed

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
 import java.util.*;
 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-
+public class Main{
+	public static void main(String[] args){
+        
+		System.out.print("Enter Your Name: ");
 		Scanner input = new Scanner(System.in);
-
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
-
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
-
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
+        System.out.println("Your Name is : " + input.next());
+        
 	}
 }
