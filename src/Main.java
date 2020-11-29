13883296791e44cc670743bddc29074e5125d027
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2020-11-29 22:42:29 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-import java.util.*;
-
 public class Main{
 	public static void main(String[] args){
-        
 		System.out.print("Enter Your Name: ");
 		Scanner input = new Scanner(System.in);
-        System.out.println("Your Name is : " + input.next());
-        
+		System.out.println("Your Name is : " + input.next());
 	}
 }
