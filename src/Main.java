b4030e5866e95ad0e7716ef142c04b097a70bb8b
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2020-11-30 21:28:06 +0530

    committed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args){
 		System.out.print("Enter Your Name: ");
-		Scanner input = new Scanner(System.in);
-		System.out.println("Hello " + input.next() + " !");
+		System.out.println("Hello " + new Scanner(System.in).next() + " !");
 	}
 }
